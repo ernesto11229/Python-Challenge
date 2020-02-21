116670b13b552091d4cbc9441b67ad8419ee5eec
ernesto11229 <ernesto11229@hotmail.com>
2020-02-21 15:16:06 -0800

    fixing path's

--- PyBank/main1.py
+++ PyBank/main1.py
@@ -2,12 +2,13 @@ import csv
 import os
 
 
-
+# list to hold number of numbers  revenue
 months=[]
 PnL=[]
 
 #with open(bank_cvs, 'r') as csvfile:
 #bank_cvs=os.path.join('C:/Users/ernes/Desktop/the class/python-challenge/pybank/budget_data.csv')
+# reader to read the csv, variable/list to hold values for total revenue, increases and decrease 
 with open("budget_data.csv", 'r') as csvfile:
     csvread=csv.reader(csvfile)
     next(csvread,None)
@@ -22,21 +23,23 @@ Gincrease=PnL[0]
 Gdecrease=PnL[0]
 TotalR=0
 
+#getting highest increase and lowest decrese and add total reve
 for R in range(len(PnL)):
-    if PnL[R] > Gincrease:
-        Gincrease > PnL[R]
+    if PnL[R] >= Gincrease:
+        Gincrease = PnL[R]
         Gmonth = months[R]
-    elif PnL[R] < Gdecrease:
+    elif PnL[R] <= Gdecrease:
         Gdecrease = PnL[R]
         LoMonth = months[R]
     TotalR += PnL[R]
 
-
+# getting average totaled 
 AverageChange=round(TotalR/TotalMonths,2)
 
-output=os.path.join('C:/Users/ernes/Desktop/the class/python-challenge/pybank/Analysis.txt')
+#pushing information to text file and removing output path, and printing 
+#output=os.path.join('C:/Users/ernes/Desktop/the class/python-challenge/pybank/Analysis.txt')
 
-with open(output,'w') as writefile:
+with open("Analysis2.txt",'w') as writefile:
     writefile.writelines('Financial Analysis\n')
     writefile.writelines('--------------------'+'\n')
     writefile.writelines('Total Months:' + str(TotalMonths)+'\n')
@@ -45,5 +48,6 @@ with open(output,'w') as writefile:
     writefile.writelines('Greatest Increase in Revenue: ' + Gmonth + ' ($' + str(Gincrease) + ')'+ '\n')
     writefile.writelines('Greatest Decrease in Revenue: ' + LoMonth + ' ($' + str(Gdecrease) + ')')
 
-with open(output,'r') as readfile:
+
+with open("Analysis2.txt",'r') as readfile:
     print(readfile.read())
